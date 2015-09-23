1ea81846fecf6e61640c8fc7709423ae6de9b494
bksasuke <student@Mac-mini.local>
2015-09-23 14:50:13 +0000

    Initial Commit

---
+++ HW_/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HW_
+//
+//  Created by student on 9/23/15.
+//  Copyright (c) 2015 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
